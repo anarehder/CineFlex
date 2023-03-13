3b6363bbb621b88e27545c7fe2593849e0a15d1d
anarehder <109766032+anarehder@users.noreply.github.com>
2023-03-13 14:38:50 -0300

    Delete src directory

--- src/pages/SeatsPage/colors.js
+++
@@ -1,6 +0,0 @@
-export const IND_BORDA = "#F7C52B";
-export const IND_FUNDO = "#FBE192";
-export const DIS_BORDA = "#808F9D";
-export const DIS_FUNDO = "#C3CFD9";
-export const SEL_BORDA = "#0E7D71";
-export const SEL_FUNDO = "#1AAE9E";
