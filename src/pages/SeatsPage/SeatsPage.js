9174a65c4fa1efe4abef216b43893440bc3e0c4a
anarehder <109766032+anarehder@users.noreply.github.com>
2023-03-13 14:40:23 -0300

    Bonus Voltar

---
+++ src/pages/SeatsPage/SeatsPage.js
@@ -0,0 +1,189 @@
+
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Seat from "./Seat";
+import Caption from "./Caption";
+
+export default function SeatsPage({setDadosCompra, dadosFilme, setDadosFilme, setVoltar}) {
+    const { idSessao } = useParams();
+    const [ids, setIds] = useState([]);
+    const [numero, setNumero] = useState([]);
+    const [name, setName] = useState("")
+    const [cpf, setCpf] = useState("")
+    const navigate = useNavigate()
+
+	useEffect(() => {
+        const url = `https://mock-api.driven.com.br/api/v8/cineflex/showtimes/${idSessao}/seats`;
+		const requisicao = axios.get(url);
+
+		requisicao.then(resposta => {
+            console.log(resposta.data);
+            setDadosFilme(resposta.data);
+            setVoltar(`/sessoes/${resposta.data.movie.id}`);
+		});
+
+        requisicao.catch(erro => {
+            console.log(erro.response.data);
+        });
+
+	}, []);
+
+	if(dadosFilme.length === 0) {
+		return "Carregando ...";
+	}
+
+    function adicionarAssento(id,assento){
+        setIds([...ids,id]);
+        setNumero([...numero,assento]);
+    }
+
+    function removerAssento(id,assento){
+        const lista = [...ids];
+        let index = lista.findIndex(elemento => elemento === id);
+        lista.splice(index,1);
+        setIds(lista);
+        const lista2 = [...numero];
+        let index2 = lista2.findIndex(elemento => elemento === assento);
+        lista2.splice(index2,1);
+        setNumero(lista2);
+    }
+
+    function enviaDados(e) {
+        e.preventDefault()
+    
+        const URL = "https://mock-api.driven.com.br/api/v8/cineflex/seats/book-many";
+        const body = { ids, name, cpf }; 
+        const dados = { numero, name, cpf };
+        setDadosCompra(dados);
+    
+        const promise = axios.post(URL, body)
+        promise.then(res => {
+          navigate("/sucesso");
+        })
+        promise.catch(err => alert(err.response.data.message))
+      }
+
+    return (
+        <PageContainer>
+            Selecione o(s) assento(s)
+            <SeatsContainer >
+            {dadosFilme.seats.map((assento) =>
+                <Seat key={assento.id} assento={assento}
+                adicionarAssento={adicionarAssento} removerAssento={removerAssento} />
+            )}
+            </SeatsContainer>
+
+            <Caption />
+
+            <FormContainer>
+            <form onSubmit={enviaDados}>
+                Nome do Comprador:
+                <input placeholder="Digite seu nome..." data-test="client-name"
+                type="text" value={name} onChange={e => setName(e.target.value)} required/>
+
+                CPF do Comprador:
+                <input placeholder="Digite seu CPF..." data-test="client-cpf"
+                type="text" value={cpf} onChange={e => setCpf(e.target.value)} required/>
+                <ButtonsContainer>
+                <button data-test="book-seat-btn" type="submit">Reservar Assento(s)</button>
+                </ButtonsContainer>
+            </form>
+            </FormContainer>
+
+            <FooterContainer data-test="footer">
+                <div>
+                    <img src={dadosFilme.movie.posterURL} alt="poster" />
+                </div>
+                <div>
+                    <p>{dadosFilme.movie.title}</p>
+                    <p>{dadosFilme.day.weekday} - {dadosFilme.name}</p>
+                </div>
+            </FooterContainer>
+
+        </PageContainer>
+    )
+}
+
+const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: 'Roboto';
+    font-size: 24px;
+    text-align: center;
+    color: #293845;
+    margin-top: 30px;
+    padding-bottom: 120px;
+    padding-top: 70px;
+`
+const SeatsContainer = styled.div`
+    width: 330px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+`
+const FormContainer = styled.div`
+    width: calc(100vw - 40px); 
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 20px 0;
+    font-size: 18px;
+    text-align: left;
+    input {
+        width: calc(100vw - 60px);
+    }
+`
+
+const ButtonsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin: 20px 0;
+    button {
+        margin: 0 auto;
+    }
+`
+
+const FooterContainer = styled.div`
+    width: 100%;
+    height: 120px;
+    background-color: #C3CFD9;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    font-size: 20px;
+    position: fixed;
+    bottom: 0;
+
+    div:nth-child(1) {
+        box-shadow: 0px 2px 4px 2px #0000001A;
+        border-radius: 3px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: white;
+        margin: 12px;
+        img {
+            width: 50px;
+            height: 70px;
+            padding: 8px;
+        }
+    }
+
+    div:nth-child(2) {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        p {
+            text-align: left;
+            &:nth-child(2) {
+                margin-top: 10px;
+            }
+        }
+    }
+`
---
+++ src/pages/SuccessPage/SuccessPage.js
@@ -0,0 +1,72 @@
+import styled from "styled-components"
+import { Link } from "react-router-dom";
+export default function SuccessPage({dadosCompra, dadosFilme, setVoltar}) {
+    setVoltar(`/assentos/${dadosFilme.id}`);
+    return (
+        <PageContainer>
+            <h1>Pedido feito <br /> com sucesso!</h1>
+
+            <TextContainer data-test="movie-info">
+                <strong><p>Filme e sessão</p></strong>
+                <p>{dadosFilme.movie.title}</p>
+                <p>{dadosFilme.day.date} - {dadosFilme.name}</p>
+            </TextContainer>
+
+            <TextContainer data-test="seats-info">
+                <strong><p>Ingressos</p></strong>
+                {dadosCompra.numero.map((i) =>
+                <p key={i} >Assento {i}</p>
+                )}
+            </TextContainer>
+
+            <TextContainer data-test="client-info">
+                <strong><p>Comprador</p></strong>
+                <p>Nome: {dadosCompra.name}</p>
+                <p>CPF: {dadosCompra.cpf}</p>
+            </TextContainer>
+            <Link to="/" data-test="go-home-btn">
+            <button>Voltar para Home</button>
+            </Link>
+        </PageContainer>
+    )
+}
+
+const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: 'Roboto';
+    font-size: 24px;
+    color: #293845;
+    margin: 30px 20px;
+    padding-bottom: 120px;
+    padding-top: 70px;
+    a {
+        text-decoration: none;
+    }
+    button {
+        margin-top: 50px;
+    }
+    h1 {
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 28px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        color: #247A6B;
+    }
+`
+const TextContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-top: 30px;
+    strong {
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
+`
